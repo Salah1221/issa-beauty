81fb5b662a75ac17711ae208e7eda7c73d7cfccc
Salah1221 <salah.najem123412345@gmail.com>
2024-09-14 09:52:48 +0300

    Hopefully a bug fix

--- server/index.js
+++ server/index.js
@@ -14,9 +14,11 @@ const getAllCategories = async () => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({ path: path.resolve(__dirname, "../.env") });
-}
+dotenv.config();
+
+// if (process.env.NODE_ENV !== "production") {
+//   dotenv.config({ path: path.resolve(__dirname, "../.env") });
+// }
 
 const app = express();
 app.use(express.json());
