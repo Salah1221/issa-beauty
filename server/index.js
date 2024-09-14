1aabae22a76c6dd47a9c1696e9d5ba78fb9d4313
Salah Najem <84938143+Salah1221@users.noreply.github.com>
2024-09-14 06:36:15 +0300

    Prevent the app from using dotenv when in production

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ const getAllCategories = async () => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: path.resolve(__dirname, "../.env") });
+  // Load environment variables from .env in development
+}
 
 const app = express();
 app.use(express.json());
