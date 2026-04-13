681699d63abea8055bf417e23b08189fc3b4607a
Salah Najem <salah.najem123412345@gmail.com>
2026-04-13 18:42:39 +0300

    Fixed the vite config and updated some backend packages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,23 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:" + PORT,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const PORT = env.PORT ? parseInt(env.PORT) : 5000;
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:" + PORT,
+        },
+      },
+    },
+  };
 });
